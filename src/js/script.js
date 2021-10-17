ad09909b9fe3994af565e729c5442d3a335a96ea
Artem Bakhmatov <ip.bakhmatov@mail.ru>
2021-10-17 16:15:52 +0300

    Вёрстка четвёртого экрана

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,11 @@ $(document).ready(function(){
         autoplaySpeed: 2500,
         arrows: false,
     });
+
+    $('.decisions__slider').slick({
+      speed: 1200,
+      adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+  });
 });

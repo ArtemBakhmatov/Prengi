9663a2e83901c754466c771200ea172f64a97712
Artem Bakhmatov <ip.bakhmatov@mail.ru>
2021-10-19 21:14:03 +0300

    Адаптация четвёртого экрана и вёрстка пятого экрана

--- src/js/script.js
+++ src/js/script.js
@@ -10,5 +10,13 @@ $(document).ready(function(){
       adaptiveHeight: true,
       prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+          }
+        }
+      ]
   });
 });
